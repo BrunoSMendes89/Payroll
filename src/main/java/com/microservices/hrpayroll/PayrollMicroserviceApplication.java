93b27277f2be062d0efd0bbee2f3fc773107878d
Bruno Escritório <brunosmendes89@gmail.com>
2022-06-29 16:35:42 -0300

    payroll created mock

---
+++ src/main/java/com/microservices/hrpayroll/PayrollMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollMicroserviceApplication.class, args);
+    }
+
+}
