1389bf582efde760aebbcdaf69cbea0281d7e281
Bruno Escritório <brunosmendes89@gmail.com>
2022-07-25 09:08:24 -0300

    Swaager documentation

--- src/main/java/com/microservices/hrpayroll/resources/PaymentResource.java
+++ src/main/java/com/microservices/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.microservices.hrpayroll.resources;
 
 import com.microservices.hrpayroll.entities.Payment;
 import com.microservices.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,16 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
+
     @GetMapping(value = "/{workerid}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerid, @PathVariable Integer days){
         Payment payment = service.getPayment(workerid,days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerid, Integer days){
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
